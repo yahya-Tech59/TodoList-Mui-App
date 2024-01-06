532d8ffc21885637fc7c1c30237af5c723aa07f8
yahya-Tech59 <yahyekade790@gmail.com>
2024-01-06 13:55:59 +0300

    first push

---
+++ Desktop/material-ui/src/App.tsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import {
+  Typography,
+  AppBar,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Toolbar,
+  Container,
+  CssBaseline,
+  Box,
+} from "@mui/material";
+import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCameraIcon />
+          <Typography variant="h6">Photo Album</Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <Box>
+          <Container maxWidth="sm">
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom
+            >
+              Photo Album
+            </Typography>
+          </Container>
+        </Box>
+      </main>
+    </>
+  );
+}
+
+export default App;
